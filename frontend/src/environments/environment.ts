c99b3853712b5c480f1d906a92907080bc6e932d
Pedro Marcelino <pmarcelino@gmail.com>
2023-03-26 11:03:21 +0100

    fix env variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-k1vtlssj33h7vu2e.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-k1vtlssj33h7vu2e.us', // the auth0 domain prefix
     audience: 'https://coffeeshop.com', // the audience set for the auth0 app
     clientId: 'E395Pys9o0DJZjv2QdPXVFc3JUgAAIfe', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
